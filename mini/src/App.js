67500f2a2951814ea3dcc15884dc7549c19348f4
Doyoung Kim <160606319+DoyoungKim273@users.noreply.github.com>
2024-05-31 16:35:59 +0900

    초기 환경  설정2

--- mini/src/App.js
+++ mini/src/App.js
@@ -3,21 +3,14 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         미니 프로젝트 시작
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="flex flex-col w-full max-w-screen-xl h-screen mx-auto overscroll-y-auto">
+      <header className="flex justify-between items-center h-20 p-10 text-xl font-bold text-black bg-slate-200">
+      <div>미니 프로젝트 시작</div>
       </header>
+      <main className="grow flex flex-col justify-center items-center"></main>
+      <footer className="flex justify-center items-center h-20 bg-slate-800 text-base text-white">
+        ⓒ 2024 Doyoung Kim. All right reserved.
+      </footer>
     </div>
   );
 }
