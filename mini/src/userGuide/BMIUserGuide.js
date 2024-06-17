540f8267efca80242bcb35f408b2a5984492dab1
Doyoung Kim <160606319+DoyoungKim273@users.noreply.github.com>
2024-06-17 17:50:55 +0900

    loading out

--- mini/src/userGuide/BMIUserGuide.js
+++ mini/src/userGuide/BMIUserGuide.js
@@ -5,7 +5,7 @@ export default function BMIUserGuide() {
   return (
     <div>
       <div className="justify-center items-center flex flex-col">
-        <img src={BMI} alt="BMI" className="justify-center w-9/12 mt-10 mb-20"></img>
+        <img src={BMI} alt="BMI" className="justify-center w-/12 mt-10 mb-20"></img>
       </div>
     </div>
   )
